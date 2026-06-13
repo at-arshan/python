8f00a99669eca53e54d03ebcdfb131e4405d8984
Arshan Atashkar <arshanatashkar86@gmail.com>
2026-06-13 20:40:54 +0330

    i added new file name numbers and i try 'split' in it and i added last updates of the main file in here too

--- python.py
+++ python.py
@@ -4,5 +4,13 @@ age = "19"
 summery = """my name is arshanatashkarand.
 im very happy to work in python"""
 print(summery[0:  :1])
-
- 
+number ="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
+print(number)
+print(number[0: 30 :3])
+nums = number.split()
+print(nums[::3])
+personal_info = summery.split()
+print(personal_info[::1])
+ages = age * 4
+print(ages)
+print(type(ages))
